1e5bf676bc01f23cd3b150849449e5ce0f646e79
agatsuma-n <agatsuma@simplemaker.co.jp>
2023-10-12 10:41:53 +0900

    ts-jest追加

--- jest.config.ts
+++ jest.config.ts
@@ -5,16 +5,10 @@
 
 export default {
 	testPathIgnorePatterns: [".d.ts", ".js"],
-
-	// Automatically clear mock calls and instances between every test
 	clearMocks: true,
-
-	// Indicates whether the coverage information should be collected while executing the test
 	collectCoverage: true,
-
-	// The directory where Jest should output its coverage files
 	coverageDirectory: "coverage",
-
-	// Indicates which provider should be used to instrument code for coverage
 	coverageProvider: "v8",
+	preset: "ts-jest",
+	testEnvironment: "node",
 };
